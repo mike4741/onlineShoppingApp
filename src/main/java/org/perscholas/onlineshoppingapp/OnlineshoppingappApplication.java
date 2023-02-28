f484554605baf0755020e314c5a9f88b099afe99
mike <me.menge47@gmail.com>
2023-02-28 08:27:21 -0600

    first commit

---
+++ src/main/java/org/perscholas/onlineshoppingapp/OnlineshoppingappApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.onlineshoppingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshoppingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshoppingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/perscholas/onlineshoppingapp/OnlineshoppingappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas.onlineshoppingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshoppingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
